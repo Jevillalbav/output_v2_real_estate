223a1a656f464dd14455a3a297aa97b040dc7e10
Jevillalbav <jevillalbav@unal.edu.co>
2024-08-29 10:52:46 -0500

    .

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
+
+st.set_page_config(layout="wide", page_title="Real Estate Report", page_icon="🏠")
+
